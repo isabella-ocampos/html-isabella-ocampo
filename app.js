d473a9c721686cb532d8162f05067445ec087d07
isabella-ocampos <123768409+isabella-ocampos@users.noreply.github.com>
2023-02-13 12:36:29 -0500

    descripción imgs

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {});
+const cardAdj = [
+  { name: "mushu", img: "images/mushu.jpg" },
+  { name: "leon", img: "images/leon.jpg" },
+  { name: "moana", img: "images/moana.jpg" },
+  { name: "nemo", img: "images/nemo.jpg" },
+  { name: "rapunzel", img: "images/rapunzel.jpg" },
+  { name: "rata", img: "images/rata.jpg" }
+];
+
+const cuadricula = document.querySelector(".cuadricula");
+const resultado = document.querySelector("#resultado");
+var cartasEscogidas = [];
+var cartasEscogidasId = [];
+var cartasGanadas = [];
